c05a3b051adc178d81cc1bbde34c6de29a0a8b92
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2015-01-28 09:42:27 -0800

    Revise OCLC query for works in an example work

--- lib/marc2linkeddata/oclc_creative_work.rb
+++ lib/marc2linkeddata/oclc_creative_work.rb
@@ -8,23 +8,32 @@ module Marc2LinkedData
 
     def get_works
       # assume an exampleOfWork can only ever link to one work?
-      q = SPARQL.parse("SELECT * WHERE { <#{@iri}> <http://schema.org/exampleOfWork> ?o }")
+      q = query_work(@iri)
       works = rdf.query(q).collect {|s| s[:o] }
       if works.empty?
-        # OCLC data is inconsistent in use of 'www' in IRI, so try again?
-        # The OclcResource coerces @iri so it includes 'www', so try without it.
+        # OCLC data is inconsistent in use of 'www.' in IRI, so try again.
+        # The OclcResource coerces @iri so it includes 'www.', so try without it.
         uri = @iri.to_s.gsub('www.','')
-        q = SPARQL.parse("SELECT * WHERE { <#{uri}> <http://schema.org/exampleOfWork> ?o }")
+        q = query_work(uri)
         works = rdf.query(q).collect {|s| s[:o] }
       end
       if works.empty?
-        # OCLC IRIs use inconsistent identifiers, sometimes the ID is an integer.
-        uri = iri.to_s.gsub(id, id.to_i.to_s)
-        q = SPARQL.parse("SELECT * WHERE { <#{uri}> <http://schema.org/exampleOfWork> ?o }")
+        # Keep the 'www.', cast the ID to an integer.
+        uri = @iri.to_s.gsub(id, id.to_i.to_s)
+        q = query_work(uri)
         works = rdf.query(q).collect {|s| s[:o] }
       end
-
+      if works.empty?
+        # Remove the 'www.' AND cast the ID to an integer.
+        uri = @iri.to_s.gsub('www.','').gsub(id, id.to_i.to_s)
+        q = query_work(uri)
+        works = rdf.query(q).collect {|s| s[:o] }
+      end
       works
+    end
+
+    def query_work(uri)
+      SPARQL.parse("SELECT * WHERE { <#{uri}> <http://schema.org/exampleOfWork> ?o }")
     end
 
     # TODO: get ISBN?
