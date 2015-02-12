b7e87a6c2eaa07f85d8b5d80811b65dbd1261bb8
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2015-02-11 18:22:39 -0800

    Pull out the gem includes for easier maintenance

--- lib/marc2linkeddata.rb
+++ lib/marc2linkeddata.rb
@@ -1,46 +1,5 @@
-require 'dotenv'
-Dotenv.load
 
-require 'addressable/uri'
-require 'json'
-require 'rest_client'
-RestClient.proxy = ENV['http_proxy'] unless ENV['http_proxy'].nil?
-
-require 'marc'
-
-require 'linkeddata'
-require 'rdf/4store'
-require 'rdf/mongo'
-
-require 'pry'
-require 'pry-doc'
-require 'ruby-progressbar'
-
-require_relative 'marc2linkeddata/configuration'
-
-require_relative 'marc2linkeddata/resource'
-require_relative 'marc2linkeddata/isni'
-require_relative 'marc2linkeddata/lib_auth'
-require_relative 'marc2linkeddata/loc'
-require_relative 'marc2linkeddata/viaf'
-
-if ENV['SUL_CAP_ENABLED'].to_s.upcase == 'TRUE'
-  require_relative 'marc2linkeddata/cap'
-end
-
-require_relative 'marc2linkeddata/oclc_resource'
-require_relative 'marc2linkeddata/oclc_identity'
-require_relative 'marc2linkeddata/oclc_creative_work'
-require_relative 'marc2linkeddata/oclc_work'
-
-require_relative 'marc2linkeddata/sparql'
-require_relative 'marc2linkeddata/sparql_dbpedia'
-require_relative 'marc2linkeddata/sparql_pubmed'
-
-require_relative 'marc2linkeddata/parseMarcAuthority'
-#require_relative 'marc2linkeddata/parseMarcCatalog'
-
-
+require_relative 'includes'
 
 module Marc2LinkedData
 
