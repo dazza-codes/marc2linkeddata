1f93c48d9e762046f01b3232df149cdc84646687
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2015-11-18 19:29:36 -0800

    Revise ctags setup

--- bin/ctags.rb
+++ bin/ctags.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
-system "find . -name '*.rb' | ctags -f .tags --languages=Ruby -L -"
+system "find ./lib/ -name '*.rb' | ctags -f .tags --languages=Ruby -L -"
+system "find .gems/ -name '*.rb' | ctags -f .gemtags --languages=Ruby -L -"
 
-if File.exist? './Gemfile'
-  require 'bundler'
-  paths = Bundler.load.specs.map(&:full_gem_path).join(' ')
-  system "ctags -R -f .gemtags --languages=Ruby #{paths}"
-end
+# if File.exist? './Gemfile'
+#   require 'bundler'
+#   paths = Bundler.load.specs.map(&:full_gem_path).join(' ')
+#   puts paths
+#   system "ctags -R -f .gemtags --languages=Ruby #{paths}"
+# end
